9a16985ac76c6e4a7eda2eb66e87d4a7f2d611c9
Bryan King Pecho <bkpecho05@gmail.com>
2023-07-28 19:03:20 +0800

    feat(Header): Update logo

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,11 @@
 import { Link, NavLink } from 'react-router-dom';
-import logo from '../assets/react.svg';
+import logo from '../assets/note.png';
 
 const Header = () => {
   return (
     <header>
       <Link to="/" className="logo">
-        <img src={logo} alt="ReactJS" />
+        <img src={logo} alt="ReactJS" />A Note To Remember
       </Link>
 
       <nav>
