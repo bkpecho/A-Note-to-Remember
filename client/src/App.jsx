21f24bfacad16d0ccee9cb5e0a0bb677524d847c
Bryan King Pecho <bkpecho05@gmail.com>
2023-07-28 17:05:40 +0800

    feat(App): Update App.jsx to include BrowserRouter, Header, Footer, and route components for Home and About pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,23 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import Footer from './components/Footer';
+import Header from './components/Header';
+import About from './routes/about/About';
+import Home from './routes/home/Home';
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </>
+  );
 }
 
 export default App;
