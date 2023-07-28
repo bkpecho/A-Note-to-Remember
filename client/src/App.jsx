6ef8dc0a459120480d0ee89b281d54afc10d6d28
Bryan King Pecho <bkpecho05@gmail.com>
2023-07-28 19:03:59 +0800

    feat(App): Update App.jsx to include routes for AddNote and ViewNote components in addition to the existing Home and About routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,9 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import About from './routes/about/About';
+import AddNote from './routes/home/AddNote';
 import Home from './routes/home/Home';
+import ViewNote from './routes/home/ViewNote';
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/add-note" element={<AddNote />} />
+          <Route path="/note/:id" element={<ViewNote />} />
         </Routes>
         <Footer />
       </Router>
