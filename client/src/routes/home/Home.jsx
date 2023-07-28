8d3ff14628007a8a4142b9412d2603d265dd9ab8
Bryan King Pecho <bkpecho05@gmail.com>
2023-07-28 17:03:31 +0800

    feat(Home): Create Home.jsx component with Notes inclusion

---
+++ client/src/routes/home/Home.jsx
@@ -0,0 +1,18 @@
+import Notes from '../../components/Notes';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam
+        quaerat unde eveniet, assumenda numquam explicabo.
+      </p>
+      <p>Lorem ipsum dolor sit amet.</p>
+
+      <Notes />
+    </div>
+  );
+};
+
+export default Home;
