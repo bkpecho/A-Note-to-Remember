35e37c584c7425f8730e75469b14b591bb6f6e86
Bryan King Pecho <bkpecho05@gmail.com>
2023-07-28 17:04:03 +0800

    feat(About): Create About.jsx component

---
+++ client/src/routes/about/About.jsx
@@ -0,0 +1,14 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet,
+        temporibus.
+      </p>
+      <p>Lorem ipsum dolor sit amet.</p>
+    </div>
+  );
+};
+
+export default About;
